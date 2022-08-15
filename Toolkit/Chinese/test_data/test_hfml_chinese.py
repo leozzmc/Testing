32f8d09862fb4ccf01ac596fe158c3e5d8889a9d
leozzmc <zzmczzmczzmc870125@gmail.com>
2022-08-15 14:28:55 +0800

    Delete Uneed folder

--- Toolkit/Chinese/test_data/test_hfml_chinese.py
+++ Toolkit/Chinese/test_data/test_hfml_chinese.py
@@ -6,7 +6,7 @@ from openpecha.formatters import HFMLFormatter
 #hfml_fn = Path("Chinese") / "test_data" / "Chinese_01.txt"
 
 # Path for testing purpose #
-hfml_fn = Path("/mnt/c/Users/Kevin/Toolkit/Chinese/test_data") / "Chinese_02.txt"
+hfml_fn = Path("/mnt/c/Users/Kevin/Testing/Tookit/Chinese/test_data") / "Chinese_02.txt"
 
 m_text = hfml_fn.read_text()
 
